357743c02a9161c6189d38ae50019ab8c1a3698f
Tanmay Yadav <tanmayyadav2323@gmail.com>
2023-06-13 10:23:14 +0530

    contract  + sidebar + navbar

---
+++ client/src/components/index.js
@@ -0,0 +1,3 @@
+export { default as NavBar } from './Navbar';
+export { default as SideBar } from './SideBar';
+export { default as CustomButton } from './CustomButton';
---
+++ client/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+
+import App from './App';
+import './index.css';
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+
+root.render(
+    <ThirdwebProvider desiredChainId={11155111}>
+        <Router>
+            <App />
+        </Router>
+    </ThirdwebProvider>
+)
