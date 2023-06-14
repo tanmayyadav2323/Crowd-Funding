16b09dd3ff6485678a2a3c065727a6546c0ef6eb
Tanmay Yadav <tanmayyadav2323@gmail.com>
2023-06-14 13:11:59 +0530

    Create Campaign

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -20,8 +20,13 @@ const CreateCampaign = () => {
         image: ''
     });
 
-    const handleSubmit = () => {
+    const handleFormFieldChange = (fieldName, e) => {
+        setForm({ ...form, [fieldName]: e.target.value });
+    }
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(form);
     }
 
     return (
@@ -39,14 +44,67 @@ const CreateCampaign = () => {
                         placeholder="John Doe *"
                         inputType="text"
                         value={form.name}
-                        handleChange={() => { }}
+                        handleChange={(e) => {
+                            handleFormFieldChange('name', e);
+                        }}
                     />
                     <FormField
                         labelName="Campaign Title"
                         placeholder="Write a title"
                         inputType="text"
                         value={form.title}
-                        handleChange={() => { }}
+                        handleChange={(e) => {
+                            handleFormFieldChange('title', e);
+                        }}
+                    />
+                </div>
+                <FormField
+                    labelName="Campaign Title"
+                    placeholder="Write your story"
+                    isTextArea={true}
+                    value={form.title}
+                    handleChange={(e) => {
+                        handleFormFieldChange('description', e);
+                    }}
+                />
+                <div className='w-full flex justify-start items-center p-4 bg-[#8c6dfd] h-[120px] rounded-[10px]'>
+                    <img src={money} alt={money} className='w-[40px] h-[40px] object-contain'></img>
+                    <h4 className='font-epilogue font-bold text-[25px] text-white ml-[20px]'>You will get the 100% of the raised amount</h4>
+                </div>
+                <div className='flex flex-wrap gap-[40px]'>
+                    <FormField
+                        labelName="Goal *"
+                        placeholder="ETH 0.50"
+                        inputType="text"
+                        value={form.target}
+                        handleChange={(e) => {
+                            handleFormFieldChange('target', e);
+                        }}
+                    />
+                    <FormField
+                        labelName="End Date"
+                        placeholder="End Date"
+                        inputType="date"
+                        value={form.deadline}
+                        handleChange={(e) => {
+                            handleFormFieldChange('deadline', e);
+                        }}
+                    />
+                    <FormField
+                        labelName="Campaign image* "
+                        placeholder="Place image URL of your campaign"
+                        inputType="url"
+                        value={form.image}
+                        handleChange={(e) => {
+                            handleFormFieldChange('image', e);
+                        }}
+                    />
+                </div>
+                <div className='flex justify-center items-center mt-[40px]'>
+                    <CustomButton
+                        btnType="submt"
+                        title="Submit new campaign"
+                        styles="bg-[#1dc071]"
                     />
                 </div>
             </form>
