318efab47aaac43f4514fe8fa21cb77dd5527af4
Tanmay Yadav <tanmayyadav2323@gmail.com>
2023-06-14 12:47:46 +0530

    FormField

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as NavBar } from './Navbar';
 export { default as SideBar } from './SideBar';
 export { default as CustomButton } from './CustomButton';
+export { default as FormField } from './FormField';
--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -3,12 +3,14 @@ import { useNavigate } from 'react-router-dom';
 import { ethers } from 'ethers';
 
 import { money } from '../assets';
-import { CustomButton } from '../components';
+import { CustomButton, FormField } from '../components';
 import { checkIfImage } from '../utils';
 
 const CreateCampaign = () => {
     const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState(false);
+
+
     const [form, setForm] = useState({
         name: '',
         title: '',
@@ -22,7 +24,6 @@ const CreateCampaign = () => {
 
     }
 
-
     return (
         <div className={`bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4`}>
             {isLoading && 'Loader...'}
@@ -32,9 +33,22 @@ const CreateCampaign = () => {
                 </h1>
             </div>
             <form onSubmit={handleSubmit} className='w-full mt-[65px] flex flex-col gap-[30px]'>
-            <div className='flex flex-wrap gap-[40px]'>
-
-            </div>
+                <div className='flex flex-wrap gap-[40px]'>
+                    <FormField
+                        labelName="Your Name *"
+                        placeholder="John Doe *"
+                        inputType="text"
+                        value={form.name}
+                        handleChange={() => { }}
+                    />
+                    <FormField
+                        labelName="Campaign Title"
+                        placeholder="Write a title"
+                        inputType="text"
+                        value={form.title}
+                        handleChange={() => { }}
+                    />
+                </div>
             </form>
         </div>
     )
