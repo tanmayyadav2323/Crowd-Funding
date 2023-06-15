780659a0ecc655b4d1b6c9682ac54b321e7b2a3e
Tanmay Yadav <tanmayyadav2323@gmail.com>
2023-06-15 20:59:24 +0530

    UI Creator + Story

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -37,7 +37,28 @@ const CampaignDetails = () => {
         </div>
       </div>
       <div className='mt-[60px] flex lg:flex-row flex-col gap-5'>
-        
+        <div className='flex-[2] flex flex-col gap-[40px]'>
+          <div>
+            <h4 className='font-epilogue font-semibold text-[18px] text-white uppercase'>Creator</h4>
+            <div className='mt-[20px] flex flex-row items-center flex-wrap gap-[14px]'>
+              <div className='w-[52px] h-[52px] flex items-center justify-center rounded-full bg-[#2c2f32] cursor-pointer'>
+                <img src={thirdweb} alt='user' className='w-[60%] h-[60%] object-contain'></img>
+              </div>
+              <div>
+                <h4 className='font-epilogue font-semibold text-[14px] text-white break-all'>{state.owner}</h4>
+                <p className='font-epilogue font-normal text-[12px] text-[#808191]'>10 Campaings</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='mt-[60px] flex lg:flex-row flex-col gap-5'>
+        <div className='flex-[2] flex flex-col gap-[40px]'>
+          <div>
+            <h4 className='font-epilogue font-semibold text-[18px] text-white uppercase'>Story</h4>
+            <p className='mt-[4px] font-epilogue font-normal text-[16px] text-[#808191] leading-[26px]'>{state.description}</p>
+          </div>
+        </div>
       </div>
     </div>
   )
