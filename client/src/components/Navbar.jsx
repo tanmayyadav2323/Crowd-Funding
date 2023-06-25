959cf542ace6fa94ba6eb32d9f62d54439d6fcd9
Tanmay Yadav <tanmayyadav2323@gmail.com>
2023-06-25 14:25:08 +0530

    commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -30,14 +30,9 @@ const NavBar = () => {
           }}
         />
       </div>
-      <Link to='/profile'>
-        <div className="w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer">
-          <img src={thirdweb} alt="user" className="w-[60%] h-[60%] object-contain" />
-        </div>
-      </Link>
       {/*Small Devices*/}
       <div className="sm:hidden flex justify-between items-center relative">
-        <div className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer">
+        <div className="w-[50px] h-[50px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer">
           <img src={logo} alt="user" className="w-[60%] h-[60%] object-contain" />
         </div>
         <img
